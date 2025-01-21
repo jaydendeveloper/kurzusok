63d6d6dbbf38240f1fa125358c252ad7f1b711a7
Patrik Gyalog <contact.jaydendev@gmail.com>
2025-01-21 13:14:56 +0100

    Add course

--- script.js
+++ script.js
@@ -1,72 +1,103 @@
 let selectedCourse = 0;
 
 function editCourse(e) {
-    selectedCourse = e.target.parentElement.querySelector('.courseId').innerText;
-    showEditModal();
+  selectedCourse =
+    e.target.parentElement.parentElement.querySelector(".courseId").innerText;
+  console.log(selectedCourse);
+  showEditModal();
 }
 
-async function getCourses(){
-    const data = await fetch("https://vvri.pythonanywhere.com/api/courses");
-    return data.json();
+function showEditModal() {
+  document.querySelector("#editModal").style.display = "flex";
+}
+function showAddModal() {
+  document.querySelector("#addModal").style.display = "flex";
 }
 
-async function printCourses(){
-    let courses = await getCourses();
+async function getCourses() {
+  const data = await fetch("https://vvri.pythonanywhere.com/api/courses");
+  return data.json();
+}
 
-    const courseList = document.querySelector("#courseList");
-    let courseHtml = "";
-    courses.forEach(element => {
-        courseHtml += `
+async function printCourses() {
+  let courses = await getCourses();
+
+  const courseList = document.querySelector("#courseList");
+  let courseHtml = "";
+  courses.forEach((element) => {
+    courseHtml += `
         <div class="course">
             <div class="courseName">${element.name}</div>
             <div class="courseId">${element.id}</div>
             <div>
                 <button class="editBtn" onClick="editCourse(event)">Szerkesztés</button>
-                <button class="deleteBtn" onClick="deleteCourse(this.parentElement.querySelector('.courseId').innerText)">Törlés</button>
+                <button class="deleteBtn" onClick="deleteCourse(this.parentElement.parentElement.querySelector('.courseId').innerText)">Törlés</button>
             </div>
-        </div>
-        `
-    });
+        </div>`;
+  });
 
-    courseList.innerHTML = courseHtml;
+  courseList.innerHTML = courseHtml;
 }
 
 printCourses();
 
-async function deleteCourse(courseId){
-    console.log("asd", courseId);
-   try{
+async function deleteCourse(courseId) {
+  console.log("asd", courseId);
+  try {
     await fetch(`https://vvri.pythonanywhere.com/api/courses/${courseId}`, {
-        method: "DELETE"
-    })
-    .then(()=>{
-        console.log("deleted course " + courseId);
-        printCourses();
-    })
-   } catch(err) {
+      method: "DELETE",
+    }).then(() => {
+      console.log("deleted course " + courseId);
+      printCourses();
+    });
+  } catch (err) {
     console.error(err);
-   }
+  }
 }
 
 const editFrom = document.querySelector("#editForm");
+const addForm = document.querySelector("#addForm");
 
-async function updateCourse(courseId){
-    console.log("asd", courseId);
-   try{
-    await fetch(`https://vvri.pythonanywhere.com/api/courses/${courseId}`, {
-        method: "PUT",
+async function updateCourse(event) {
+  console.log("asd", selectedCourse);
+  event.preventDefault();
+  try {
+    await fetch(
+      `https://vvri.pythonanywhere.com/api/courses/${selectedCourse}`,
+      {
+        method: "PATCH",
         body: JSON.stringify({
-            name: editFrom.querySelector("#name").value
+          name: editFrom["name"].value,
         }),
         headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    .then(()=>{
-        console.log("updated course " + courseId);
-        printCourses();
-    })
-   } catch(err) {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      console.log("updated course " + selectedCourse);
+      printCourses();
+    });
+  } catch (err) {
     console.error(err);
-   }
+  }
+}
+
+async function addCourse(event) {
+  event.preventDefault();
+  try {
+    await fetch(`https://vvri.pythonanywhere.com/api/courses`, {
+      method: "POST",
+      body: JSON.stringify({
+        name: addForm["name"].value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      console.log("added course");
+      printCourses();
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
